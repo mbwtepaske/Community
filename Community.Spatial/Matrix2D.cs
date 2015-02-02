1256cb92a7bea1d3a832585d476422abf3fcfc72
Thijs <mbwtepaske@gmail.com>
2015-02-02 14:57:56 +0100

    Missing file

---
+++ Community.Spatial/Matrix2D.cs
@@ -0,0 +1,17 @@
+﻿using Matrix = MathNet.Numerics.LinearAlgebra.Matrix<double>;
+
+namespace System.Spatial
+{
+  public static class Matrix2D
+  {
+    public const Int32 Order = 2;
+
+    /// <summary>
+    /// Creates a 2 x 2 identity-matrix.
+    /// </summary>
+    public static Matrix Identity()
+    {
+      return Matrix.Build.DenseIdentity(Order);
+    }
+  }
+}
